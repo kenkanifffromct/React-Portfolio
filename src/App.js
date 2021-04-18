1d69c4ab106d38a9399474561bbe2b6363053f1b
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-04-18 15:10:16 -0400

    created utils, components and pages folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route exact path="/About" component={About} />
+            <Route exact path="/Contact" component={Contact} />
+            <Route exact path="/posts/Portfolio" component={Portfolio} />
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
