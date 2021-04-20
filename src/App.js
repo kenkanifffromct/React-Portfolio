84f433112101936d48a83f596716f349225af113
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-04-19 20:05:53 -0400

    Removed files and starting over

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Navbar from "./components/Navbar";
-import Contact from "./pages/Contact";
+// import Router from "react-router-dom";
+
 
 function App() {
   return (
-    <Router>
-      <div>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={About} />
-            <Route exact path="/About" component={About} />
-            <Route exact path="/Contact" component={Contact} />
-            <Route exact path="/posts/Portfolio" component={Portfolio} />
-          </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Yooooooo. Is it working?</h1>
+    </div>
   );
 }
 
