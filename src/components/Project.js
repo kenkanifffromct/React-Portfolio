53d6236adf74a71b2060c132e3dc8c5c29343500
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-04-21 22:01:52 -0400

    updated layout

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 function Project() {
     return (
-        <div className="card">
-  <div className="card-body">
+        <div className="card-group">
+  <div className="card-body col-sm-4">
     <h5 className="card-title">Fitness Tracker</h5>
     <p className="card-text">Track your daily workouts!</p>
     <a href="https://github.com/kenkanifffromct/Workout-Tracker" className="btn btn-primary">Github Repo</a>
     <a href="https://serene-anchorage-94653.herokuapp.com/" className="btn btn-primary">Run App</a>
   </div>
-  <div className="card-body">
+  <div className="card-body col-sm-4">
     <h5 className="card-title">Weather Memory</h5>
     <p className="card-text">Check what the weather was!</p>
     <a href="https://github.com/jgibsone4/weatherMemory" className="btn btn-primary">Github Repo</a>
     <a href="https://jgibsone4.github.io/weatherMemory/" className="btn btn-primary">Run App</a>
   </div>
-  <div className="card-body">
+  <div className="card-body col-sm-4">
     <h5 className="card-title">Budget Tracker</h5>
     <p className="card-text">Track your finances!</p>
     <a href="https://github.com/kenkanifffromct/Budget-Tracker" className="btn btn-primary">Github Repo</a>
