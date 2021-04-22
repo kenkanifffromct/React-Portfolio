2b3658b24a0990e31e6835d4d4a018e0c18f7156
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-04-21 21:49:21 -0400

    updated projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,7 +9,20 @@ function Project() {
     <a href="https://github.com/kenkanifffromct/Workout-Tracker" className="btn btn-primary">Github Repo</a>
     <a href="https://serene-anchorage-94653.herokuapp.com/" className="btn btn-primary">Run App</a>
   </div>
+  <div className="card-body">
+    <h5 className="card-title">Weather Memory</h5>
+    <p className="card-text">Check what the weather was!</p>
+    <a href="https://github.com/jgibsone4/weatherMemory" className="btn btn-primary">Github Repo</a>
+    <a href="https://jgibsone4.github.io/weatherMemory/" className="btn btn-primary">Run App</a>
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Budget Tracker</h5>
+    <p className="card-text">Track your finances!</p>
+    <a href="https://github.com/kenkanifffromct/Budget-Tracker" className="btn btn-primary">Github Repo</a>
+    <a href="https://guarded-atoll-16149.herokuapp.com/" className="btn btn-primary">Run App</a>
+  </div>
         </div>
+        
     )
 }
 
